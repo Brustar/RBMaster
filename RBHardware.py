78bc8da59d3b5560918ae72487f32ec5a29b2d53
“Brustar” <yeals2@gmail.com>
2016-11-30 09:10:07 +0800

    hardware

--- RBHardware.py
+++ RBHardware.py
@@ -1,12 +1,12 @@
 import serial
+import binascii
 from time import sleep
 
 class RBHardware(object):
 	def __init__(self):
-		self.serial = serial.Serial('/dev/ttyUSB0', 9600, timeout=0.5)
+		self.serial = serial.Serial('/dev/tty.usbserial', 9600, timeout=0.5)
 
 	def recv(self,length):
-		data
 		while True:
 			data = self.serial.read(length)
 			if data == '':
@@ -16,8 +16,24 @@ class RBHardware(object):
 			sleep(0.02)
 		return data
 
-	def readEclectricity(self):
-		return self.recv(20)
+	def readElectricity(self):
+		return self.recv(8)
 
 	def switchAircon(self,data):
-		self.serial.write(data)
+		if self.serial.isOpen():
+			self.serial.write(data)
+
+	def close(self):
+		self.serial.close()
+
+
+if __name__ == "__main__":
+	cmds = ["F501FFFE006400A9", "F501FFFE016400A8" , "F501FFFE026400A7","f501fffe0b64009e","f501fffe046400a5","f501fffe036400a6"]
+	hardware = RBHardware()
+	for i in range(0 , 20):
+		for cmd in cmds:
+			hardware.switchAircon(binascii.unhexlify(cmd))
+			sleep(0.1)
+	readcmd = hardware.readElectricity()
+	print binascii.b2a_hex(readcmd)
+	hardware.close()
